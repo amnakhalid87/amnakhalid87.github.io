bb9fad87f77f68fb055bbdb613af21e0acdf86d8
Amna Khalid <162368181+Amnakhalid127@users.noreply.github.com>
2026-02-12 06:53:29 -0800

    Updated portfolio with build files

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,7 +32,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b8958c7c299abe31afd7071cebce8503
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "7ef114c62d65b69e9632e984e5360257",
+"flutter_bootstrap.js": "5df01f55a83ec3fbc96b7421331abf45",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
